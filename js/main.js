cfe0f64dfda722761f2df45a47ad91a11baf8fc6
silentsoft <hyesung.lee@silentsoft.org>
2019-09-30 20:01:36 +0900

    add SHA256 checksum to archives

--- js/main.js
+++ js/main.js
@@ -51,4 +51,21 @@ $(function() {
         });
     }
 
+    $('#checksumModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget);
+        var name = button.data('name');
+        var sha256 = button.data('sha256');
+
+        var modal = $(this);
+        modal.find('.modal-title').text(name);
+        modal.find('.modal-body input').val(sha256);
+
+        $('#copy').on('click', function (e) {
+            modal.find('.modal-body input').select();
+            document.execCommand('copy');
+
+            $('#checksumModal').modal('hide');
+        });
+    })
+
 });
